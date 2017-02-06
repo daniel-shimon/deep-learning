Fix run Refactor numerical gradient

chained_operations.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def run(return_values, feed_dict):
+def run(return_values, feed_dict=None):
+    feed_dict = feed_dict or {}
     for operation, value in feed_dict.items():
         operation.run(value)
 
@@ -9,15 +10,19 @@ def run(return_values, feed_dict):
         ret = []
         for operation in return_values:
             ret.append(operation.output)
+            operation.backwards()
         return tuple(ret)
     elif isinstance(return_values, dict):
         ret = {}
         for key, operation in return_values.items():
             ret[key] = operation.output
+            operation.backwards()
         return ret
 
     operation = return_values
-    return operation.output
+    ret = operation.output
+    operation.backwards()
+    return ret
 
 
 class ChainedOperation(object):
@@ -90,7 +95,7 @@ class Variable(ChainedOperation):
         return self.value
 
     def calc_backwards(self):
-        return 1
+        return np.ones_like(self.value, dtype=np.float64)
 
     def run(self, value):
         self.value = value
@@ -99,13 +104,18 @@ class Variable(ChainedOperation):
 
 class Gradient(ChainedOperation):
     def __init__(self, operation):
-        super(Gradient, self).__init__([operation])
+        super(Gradient, self).__init__([])
+        operation.input_objects.append(self)
 
     def calc_forwards(self, inputs):
-        return self.input_objects[0].grad
+        return 1
 
     def calc_backwards(self):
         return 1
+
+    def backwards(self, output_object=None):
+        super(Gradient, self).backwards(output_object)
+        self.output = self.grad
 
 
 class Sum(ChainedOperation):

optimizers.py
@@ -3,16 +3,16 @@ import math
 import numpy as np
 
 
-def numeric_gradient(params, f):
-    params = np.asarray(params)
-    d_params = np.zeros_like(params)
-    fx = f(params)
-    for i in xrange(params.shape[0]):
-        like_params = np.copy(params)
-        dx = 1e-9
-        like_params[i, ] += dx
+def numeric_gradient(x, f):
+    x = np.asarray(x)
+    d_params = np.zeros_like(x)
+    fx = f(x)
+    for i in xrange(x.shape[0]):
+        like_x = np.copy(x)
+        dx = like_x[i, ] * 1e-8
+        like_x[i, ] += dx
 
-        derivative = (f(like_params) - fx) / dx
+        derivative = (f(like_x) - fx) / dx
         if np.shape(derivative) == ():
             d_params[i, ] = derivative
         else:

playground.py
@@ -18,6 +18,8 @@ y = op.Sum(op.Exp(x))
 
 g = op.Gradient(x)
 
-print op.run([g, y], {x: np.array([1, 2, 3])})
+op.run(y, {x: np.array([1.0, 2.0, 3.0])})
 
-print optimizers.numeric_gradient(np.array([1,2,3]), lambda inp: op.run(y, {x: inp}))
+print op.run(g)
+
+print optimizers.numeric_gradient(np.array([1.0, 2.0, 3.0]), lambda inp: op.run(y, {x: inp}))