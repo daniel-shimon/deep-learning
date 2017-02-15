Fixes for backpropagation Fix for numeric_gradient

chained_operations.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def run(return_values, feed_dict=None):
+def run(return_values, feed_dict=None, backwards=True):
     feed_dict = feed_dict or {}
     for operation, value in feed_dict.items():
         operation.run(value)
@@ -10,18 +10,21 @@ def run(return_values, feed_dict=None):
         ret = []
         for operation in return_values:
             ret.append(operation.output)
-            operation.backwards()
+            if backwards:
+                operation.backwards()
         return tuple(ret)
     elif isinstance(return_values, dict):
         ret = {}
         for key, operation in return_values.items():
             ret[key] = operation.output
-            operation.backwards()
+            if backwards:
+                operation.backwards()
         return ret
 
     operation = return_values
     ret = operation.get_output()
-    operation.backwards()
+    if backwards:
+        operation.backwards()
     return ret
 
 
@@ -31,7 +34,7 @@ class ChainedOperation(object):
         self.output_objects = []
         self.inputs_ready = {}
         self.outputs_ready = {}
-        self.inputs = None
+        self.inputs = []
         self.output = None
         self.grad = 0
 
@@ -48,7 +51,7 @@ class ChainedOperation(object):
         self.inputs_ready[input_object] = True
 
         if self.all_inputs_ready():
-            self.inputs = []
+            self.inputs.clear()
             for i in self.input_objects:
                 if isinstance(i, ChainedOperation):
                     self.inputs.append(i.get_output())
@@ -59,14 +62,19 @@ class ChainedOperation(object):
             for o in self.output_objects:
                 o.forwards(self)
 
-            # Clear backwards variables
+            # Clear variables
             self.grad = 0
             for key in self.outputs_ready.keys():
                 self.outputs_ready[key] = False
+            for key in self.inputs_ready.keys():
+                self.inputs_ready[key] = False
 
     def backwards(self, output_object=None):
+        if len(self.inputs) != len(self.input_objects):
+            raise ValueError('cannot preform backwards pass before full forwards pass')
+
         if output_object is None:
-            self.grad = 1
+            self.grad = np.ones_like(self.output)
         else:
             self.outputs_ready[output_object] = True
             self.grad += output_object.get_grad(self)
@@ -83,14 +91,15 @@ class ChainedOperation(object):
         return self.grad * self.calc_backwards(input_object)
 
     def all_inputs_ready(self):
-        for state in self.inputs_ready.values():
-            if state is False:
-                return False
-        return True
+        return self.all_ready(self.inputs_ready)
 
     def all_outputs_ready(self):
-        for o in self.outputs_ready.items():
-            if o is False:
+        return self.all_ready(self.outputs_ready)
+
+    @staticmethod
+    def all_ready(l):
+        for state in l.values():
+            if state is False:
                 return False
         return True
 
@@ -101,9 +110,9 @@ class ChainedOperation(object):
         raise NotImplementedError('Abstract class')
 
 
-class SingleInputChainedOperation(ChainedOperation):
+class UnaryChainedOperation(ChainedOperation):
     def __init__(self, x):
-        super(SingleInputChainedOperation, self).__init__([x])
+        super(UnaryChainedOperation, self).__init__([x])
 
     def calc_forwards(self, inputs):
         return self.calc_forwards_single(inputs[0])
@@ -118,6 +127,23 @@ class SingleInputChainedOperation(ChainedOperation):
         raise NotImplementedError('Abstract class')
 
 
+class BinaryChainedOperation(ChainedOperation):
+    def __init__(self, a, b):
+        super(BinaryChainedOperation, self).__init__([a, b])
+
+    def calc_forwards(self, inputs):
+        return self.calc_forwards_dual(inputs[0], inputs[1])
+
+    def calc_backwards(self, input_object):
+        return self.calc_backwards_dual(input_object, self.inputs[0], self.inputs[1])
+
+    def calc_forwards_dual(self, a, b):
+        raise NotImplementedError('Abstract class')
+
+    def calc_backwards_dual(self, input_object, a, b):
+        raise NotImplementedError('Abstract class')
+
+
 class Placeholder(ChainedOperation):
 
     def __init__(self):
@@ -128,10 +154,10 @@ class Placeholder(ChainedOperation):
         return self.value
 
     def calc_backwards(self, _):
-        return np.ones_like(self.value, dtype=np.float64)
+        return 1
 
     def run(self, value):
-        self.value = np.asarray(value)
+        self.value = np.asmatrix(value)
         self.forwards(self)
 
 
@@ -152,7 +178,7 @@ class Gradient(ChainedOperation):
         self.output = self.grad
 
 
-class Log(SingleInputChainedOperation):
+class Log(UnaryChainedOperation):
     def calc_forwards_single(self, x):
         return np.log(x)
 
@@ -160,47 +186,38 @@ class Log(SingleInputChainedOperation):
         return 1 / x
 
 
-class Sum(SingleInputChainedOperation):
+class Sum(UnaryChainedOperation):
     def calc_forwards_single(self, x):
         return np.sum(x)
 
     def calc_backwards_single(self, x):
-        return 1
+        return np.ones_like(x)
 
 
-class Mul(ChainedOperation):
-    def __init__(self, a, b):
-        super(Mul, self).__init__([a, b])
+class Mul(BinaryChainedOperation):
+    def calc_forwards_dual(self, a, b):
+        return np.multiply(a, b)
 
-    def calc_forwards(self, inputs):
-        return np.multiply(inputs[0], inputs[1])
-
-    def calc_backwards(self, input_object):
-        for i in self.input_objects:
-            if i is not input_object:
-                if isinstance(i, ChainedOperation):
-                    return i.get_output()
-
-                return i
+    def calc_backwards_dual(self, input_object, a, b):
+        if self.input_objects.index(input_object) == 0:
+            return b
+        return a
 
 
-class Dot(ChainedOperation):
-    def __init__(self, a, b):
-        super(Dot, self).__init__([a, b])
+class Dot(BinaryChainedOperation):
+    def get_grad(self, input_object):
+        if self.input_objects.index(input_object) == 0:
+            return np.dot(self.grad, np.transpose(self.inputs[1]))
+        return np.dot(np.transpose(self.inputs[0]), self.grad)
 
-    def calc_forwards(self, inputs):
-        return np.dot(inputs[0], inputs[1])
+    def calc_forwards_dual(self, a, b):
+        return np.dot(a, b)
 
-    def calc_backwards(self, input_object):
-        for i in self.input_objects:
-            if i is not input_object:
-                if isinstance(i, ChainedOperation):
-                    return i.get_output()
-
-                return i
+    def calc_backwards_dual(self, input_object, a, b):
+        pass
 
 
-class Reciprocal(SingleInputChainedOperation):
+class Reciprocal(UnaryChainedOperation):
     def calc_forwards_single(self, x):
         return 1 / x
 
@@ -208,7 +225,7 @@ class Reciprocal(SingleInputChainedOperation):
         return - 1 / np.square(x)
 
 
-class Exp(SingleInputChainedOperation):
+class Exp(UnaryChainedOperation):
     def calc_forwards_single(self, x):
         if hasattr(x, 'max'):
             return np.exp(x - x.max())

linear_classifier.py
@@ -1,7 +1,9 @@
 import numpy as np
+import itertools as it
 
 import chained_operations as op
 import data_utils
+from optimizers import numeric_gradient
 
 label_names = data_utils.Cifar10.load_labels()
 
@@ -16,26 +18,29 @@ g = op.Gradient(w)
 y = op.Dot(x, w)
 
 y_ = op.Placeholder()
-loss = op.Mul(-1, op.Log(op.Mul(op.Dot(y_, op.Exp(y)), op.Reciprocal(op.Sum(op.Exp(y))))))
+loss = op.Mul(-1, op.Sum(y))
 
 batch_size = 10
 w_values = np.random.randn(1024 * 3, 10) * 1e-4
 
-counter = 0
-for mini_batch_x, mini_batch_y_ in zip(
+for i, mini_batch_x, mini_batch_y_ in zip(
+        it.count(),
         data_utils.batches(data, batch_size),
         data_utils.batches(labels, batch_size)):
     batch_loss = 0
     batch_grad = 0
+    num = 0
     for i in range(batch_size):
-        batch_loss += op.run(loss, feed_dict={x: mini_batch_x[i],
-                                              y_: mini_batch_y_[i],
+        sample_y_ = np.zeros((len(label_names), 1))
+        sample_y_[mini_batch_y_[i]] = 1
+        batch_loss += op.run(loss, feed_dict={x: np.asmatrix(mini_batch_x[i]),
+                                              y_: sample_y_.T,
                                               w: w_values})
         batch_grad += op.run(g)
 
     batch_loss /= batch_size
     batch_grad /= batch_size
+    num /= batch_size
 
-    print('%d loss - %f' % (counter, batch_loss))
+    print('%d loss - %f' % (i, batch_loss))
     w_values -= batch_grad * 0.01
-    counter += 1

optimizers.py
@@ -4,20 +4,35 @@ import numpy as np
 
 
 def numeric_gradient(x, f):
-    x = np.asarray(x, np.float64)
-    d_params = np.zeros_like(x)
+    x = np.asmatrix(x, np.float64)
+    d_x = np.zeros_like(x)
     fx = f(x)
-    for i in range(x.shape[0]):
+    r = [None]
+    if np.shape(fx) != () and np.shape(fx) != np.shape(d_x):
+        for rank in range(len(np.shape(fx))):
+            if np.shape(fx)[rank] != np.shape(d_x)[rank]:
+                r[0] = rank
+                break
+
+    it = np.nditer(d_x, flags=['multi_index'])
+    while not it.finished:
         like_x = np.copy(x)
-        dx = like_x[i, ] * 1e-8
-        like_x[i, ] += dx
+        dx = like_x[it.multi_index] * 1e-8
+        like_x[it.multi_index] += dx
 
         derivative = (f(like_x) - fx) / dx
         if np.shape(derivative) == ():
-            d_params[i, ] = derivative
-        else:
-            d_params[i, ] = derivative[i, ]
-    return d_params
+            d_x[it.multi_index] = derivative
+        elif np.shape(derivative) == np.shape(d_x):
+            d_x[it.multi_index] = derivative[it.multi_index]
+        elif r[0] is not None:
+            for i in range(np.shape(derivative)[r[0]]):
+                location = list(it.multi_index)
+                location[r[0]] = i
+                d_x[it.multi_index] += derivative[tuple(location)]
+
+        it.iternext()
+    return d_x
 
 
 class BaseOptimizer(object):