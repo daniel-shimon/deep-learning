Only get_output from ChainedOperation objects Use random weights from -1 to 1 Optimizer forwards variable layers Data_util normalization fix VariableLayer fith lock/unlock methods Layers' set_inputs() method Add graph clear method Prepare for deep dream

chained_operations.py
@@ -9,23 +9,34 @@ def run(return_values, feed_dict=None, backwards=True):
     if isinstance(return_values, (list, tuple)):
         ret = []
         for operation in return_values:
-            ret.append(operation.get_output())
+            if isinstance(operation, ChainedOperation):
+                output = operation.get_output()
+            else:
+                output = operation
+            ret.append(output)
             if backwards:
                 operation.backwards()
         return tuple(ret)
     elif isinstance(return_values, dict):
         ret = {}
         for key, operation in return_values.items():
-            ret[key] = operation.get_output()
+            if isinstance(operation, ChainedOperation):
+                output = operation.get_output()
+            else:
+                output = operation
+            ret[key] = output
             if backwards:
                 operation.backwards()
         return ret
 
     operation = return_values
-    ret = operation.get_output()
+    if isinstance(operation, ChainedOperation):
+        output = operation.get_output()
+    else:
+        output = operation
     if backwards:
         operation.backwards()
-    return ret
+    return output
 
 
 class ChainedOperation(object):
@@ -39,8 +50,9 @@ class ChainedOperation(object):
         self.grad = 0
 
         for i in self.input_objects:
-            if isinstance(i, ChainedOperation) and not isinstance(i, Variable):
-                self.inputs_ready[i] = False
+            if isinstance(i, ChainedOperation):
+                if not isinstance(i, Variable):
+                    self.inputs_ready[i] = False
                 i.add_output(self)
 
     def add_output(self, output_object):
@@ -75,7 +87,8 @@ class ChainedOperation(object):
 
     def backwards(self, output_object=None):
         if len(self.inputs) != len(self.input_objects):
-            raise ValueError('cannot preform backwards pass before full forwards pass')
+            raise ValueError('cannot preform backwards pass before full forwards pass,'
+                             'have you missed placeholder values in the feed_dict?')
 
         if output_object is None:
             if not np.any(self.grad):
@@ -167,8 +180,12 @@ class Placeholder(ChainedOperation):
         self.forwards()
 
 
+def random_weight(*args):
+    return (np.random.rand(*args) - 0.5) * 2
+
+
 class Variable(Placeholder):
-    def __init__(self, shape, random_func=np.random.rand):
+    def __init__(self, shape, random_func=random_weight):
         super(Variable, self).__init__()
         self.shape = shape
         self.random_func = random_func

chained_optimizers.py
@@ -8,12 +8,16 @@ class ChainedOptimizer(object):
         self.loss = loss
         self.direction = -1 if minimize else 1
 
+        self.variable_layers = []
         self.variables = self.find_variables(loss)
         if not self.variables:
             raise ValueError('no variables found to optimize')
 
-    def step(self, feed_dict):
+    def step(self, feed_dict=None):
+        feed_dict = feed_dict or {}
         self.reset_grads()
+        for layer in self.variable_layers:
+            layer.forwards()
         loss = op.run(self.loss, feed_dict)
 
         for variable in self.variables:
@@ -27,9 +31,10 @@ class ChainedOptimizer(object):
     def update(self, variable, gradient):
         raise NotImplementedError()
 
-    @staticmethod
-    def find_variables(chained_operation):
+    def find_variables(self, chained_operation):
         if isinstance(chained_operation, op.ChainedOperation):
+            if isinstance(chained_operation, nn.VariableLayer):
+                self.variable_layers.append(chained_operation)
             if isinstance(chained_operation, op.Variable):
                 return [chained_operation]
 
@@ -37,7 +42,7 @@ class ChainedOptimizer(object):
             if isinstance(chained_operation, nn.Layer):
                 variables.extend(chained_operation.get_variables())
             for input_object in chained_operation.input_objects:
-                variables.extend(ChainedOptimizer.find_variables(input_object))
+                variables.extend(self.find_variables(input_object))
             return variables
         return []
 

data_utils.py
@@ -39,14 +39,14 @@ class Cifar10(object):
 
     @staticmethod
     def show_img_array(flat, normalized=True):
-        if normalized:
-            flat += 0.5
-            flat *= 255
-
         img_array = np.zeros((32, 32, 3))
         for channel in range(3):
             for row in range(32):
                 for collum in range(32):
                     img_array[row, collum, channel] = flat[channel * 1024 + row * 32 + collum]
+        if normalized:
+            img_array -= img_array.min()
+            img_array /= img_array.max()
+            img_array *= 255
         with Image.fromarray(img_array.astype(np.uint8)) as img:
             img.show()

linear_classifier.py
@@ -7,9 +7,13 @@ import data_utils
 import plotting
 import chained_optimizers as optimizers
 
+print('get data')
+
 label_names = data_utils.Cifar10.load_labels()
 
 data, labels = data_utils.Cifar10.get_batch(1, raw=False)
+
+print('build model')
 
 x = nn.InputLayer(1024 * 3)
 
@@ -18,10 +22,12 @@ y = nn.Dense(x, 10)
 y_ = op.Placeholder()
 loss = nn.Softmax(y, y_)
 
-batch_size = 10
+print('train model')
 
-optimizer = optimizers.Adagrad(loss)
+batch_size = 100
+
 graph = plotting.Graph('loss')
+optimizer = optimizers.Adagrad(loss)
 
 for count, mini_batch_x, mini_batch_y_ in zip(
         it.count(),
@@ -33,3 +39,28 @@ for count, mini_batch_x, mini_batch_y_ in zip(
 
     print('%d loss - %f' % (count, batch_loss))
     graph.maybe_add(count, count, batch_loss, True)
+
+    if batch_loss < 4:
+        break
+
+
+print('deep dream')
+
+images = nn.VariableLayer(1, 1024 * 3)
+y.set_inputs(images)
+y.lock()
+
+y.set_inputs(x)
+
+truths = np.zeros((1, 10))
+truths[0][1] = 1
+
+graph.clear()
+optimizer = optimizers.Adagrad(loss)
+
+for i in range(15):
+    loss = np.mean(optimizer.step({y_: truths}))
+    print('%d loss - %f' % (i, loss))
+    graph.maybe_add(i, i, loss, plot=True)
+
+print('done')